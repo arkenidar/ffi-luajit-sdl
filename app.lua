7a5e355c86c06dc14c9e05bedaac65620fa9cea9
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-11-12 12:18:24 +0100

    rewritten cleanly, based on previous experience and furthered understanding

--- app.lua
+++ app.lua
@@ -1,37 +1,20 @@
-
 local ffi = require("ffi")
-
--- Parse the C API header
--- It's generated with:
---
---     echo '#include <SDL2/SDL.h>' >> includes.c
---     gcc -E includes.c | grep -v '^#' > ffi_defs.h
---
-
 ffi.cdef( io.open('ffi_defs.h','r'):read('*a') )
-
 local SDL = ffi.load('SDL2')
 
 SDL.SDL_Init(0)
 local window = SDL.SDL_CreateWindow("title", 50,50, 400,300, 0)
-local renderer = SDL.SDL_CreateRenderer(window, -1, 0)
-
 local window_surface = SDL.SDL_GetWindowSurface(window)
 
---********************************************
+function image_load(name)
+  local file = SDL.SDL_RWFromFile(name..".bmp", "rb")
+  return SDL.SDL_LoadBMP_RW(file, 1)
+end
+function image_transparency(image_surface,rgb)
+  local key = SDL.SDL_MapRGB(window_surface.format,rgb[1],rgb[2],rgb[3])
+  SDL.SDL_SetColorKey(image_surface, SDL.SDL_TRUE, key)  
+end
 
--- load assets
-
-local file = SDL.SDL_RWFromFile("transparence.bmp", "rb")
-local image_surface = SDL.SDL_LoadBMP_RW(file, 1)
-
----local image_surface = SDL.SDL_LoadBMP("transparence.bmp")
-local window_surface = SDL.SDL_GetWindowSurface(window)
-local rgb = {0,0,255} -- key color (blue is transparent)
-local key = SDL.SDL_MapRGB(window_surface.format,rgb[1],rgb[2],rgb[3])
-SDL.SDL_SetColorKey(image_surface, SDL.SDL_TRUE, key)
-
--- utility
 function rect_from_xywh(xywh)
 if xywh == nil then return nil end
 local rect = ffi.new('SDL_Rect')
@@ -42,24 +25,17 @@ rect.h = xywh[4]
 return rect
 end
 
-----
-  function draw_rect_renderer(rgb, xywh)
-    SDL.SDL_SetRenderDrawColor(renderer,rgb[1],rgb[2],rgb[3],255)
-    SDL.SDL_RenderFillRect(renderer, rect_from_xywh(xywh))
-  end
+function surface_draw_rect(rgb, xywh)
+SDL.SDL_FillRect(window_surface, rect_from_xywh(xywh), SDL.SDL_MapRGB(window_surface.format,rgb[1],rgb[2],rgb[3]))
+end
 
-  function draw_rect_surface(rgb, xywh)
-    SDL.SDL_FillRect(window_surface, rect_from_xywh(xywh), SDL.SDL_MapRGB(window_surface.format,rgb[1],rgb[2],rgb[3]))
-  end
----
-  function draw_image_texture(image_texture, xywh)
-    SDL.SDL_RenderCopy(renderer, image_texture, nil, rect_from_xywh(xywh))
-  end
+function surface_draw_image(image_surface, xywh)
+SDL.SDL_UpperBlitScaled(image_surface, nil, window_surface, rect_from_xywh(xywh) )
+end
 
-  function draw_image_surface(image_surface, xywh)
-    SDL.SDL_UpperBlitScaled(image_surface, nil, window_surface, rect_from_xywh(xywh) )
-  end
---***********************************
+local image_surface = image_load("transparence")
+local rgb = {0,0,255} -- key color (blue is transparent)
+image_transparency(image_surface,rgb)
 
 local event = ffi.new("SDL_Event")
 local looping = true
@@ -75,15 +51,14 @@ while looping do
   local rgb={255,255,0}
   local xywh={0,0, 100, 100}
 
-  --draw_rect_renderer(rgb, xywh)
-  draw_rect_surface(rgb, xywh)
-  
-  --draw_image_texture(image_texture, xywh)
-  draw_image_surface(image_surface, xywh)
+  surface_draw_rect(rgb, xywh)
 
-  --SDL.SDL_RenderPresent(renderer)
+  surface_draw_image(image_surface, xywh)
+
   SDL.SDL_UpdateWindowSurface(window)
 
 end
+
+SDL.SDL_FreeSurface(image_surface)
 
 SDL.SDL_Quit()
