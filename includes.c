a4b81b72340bffef04ab935720202bc330b224f3
Dario Cangialosi Arkenidar (DCA) <dario.cangialosi@gmail.com>
2022-11-14 15:59:27 +0100

    "opengl in sdl" example (minimal proof of concept)

---
+++ includes.c
@@ -0,0 +1,8 @@
+/* this is includes.c, processed with:
+
+gcc -E includes.c | grep -v '^#' > ffi_defs_gl.h
+*/
+#include <SDL2/SDL.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
---
+++ opengl.lua
@@ -0,0 +1,90 @@
+local ffi = require("ffi")
+
+local SDL = ffi.load('SDL2')
+local GL = ffi.load('GL')
+local GLU = ffi.load('GLU')
+
+--[[
+/* this is includes.c, processed with:
+
+gcc -E includes.c | grep -v '^#' > ffi_defs_gl.h
+*/
+#include <SDL2/SDL.h>
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+]]
+ffi.cdef( io.open('ffi_defs_gl.h','r'):read('*a') )
+
+SDL.SDL_Init(0)
+
+--[[
+(SublimeText's "find in files" shows this)
+
+/usr/include/SDL2/SDL_video.h:
+   98  {
+   99      SDL_WINDOW_FULLSCREEN = 0x00000001,         /**< fullscreen window */
+  100:     SDL_WINDOW_OPENGL = 0x00000002,             /**< window usable with OpenGL context */
+  101      SDL_WINDOW_SHOWN = 0x00000004,              /**< window is visible */
+  102      SDL_WINDOW_HIDDEN = 0x00000008,             /**< window is not visible */
+]]
+local SDL_WINDOW_OPENGL = 2
+local window_width, window_height = 400, 300
+local window = SDL.SDL_CreateWindow("opengl in sdl", 50,50, window_width, window_height, SDL_WINDOW_OPENGL)
+
+context = SDL.SDL_GL_CreateContext(window)
+
+-- #define GL_COLOR_BUFFER_BIT			0x00004000
+local GL_COLOR_BUFFER_BIT	= 0x00004000
+
+function update(mouse_position,mouse_down)
+	-- to be defined
+end
+
+function draw()
+  GL.glViewport(0, 0, window_width, window_height)
+  GL.glClearColor(1.0, 0.0, 1.0, 0.0)
+  GL.glClear(GL_COLOR_BUFFER_BIT)
+
+	GL.glBegin(4) --   220: #define GL_TRIANGLES				0x0004
+  GL.glColor3f(0.1, 0.2, 0.3)
+  GL.glVertex3f(0, 0, 0)
+  GL.glVertex3f(1, 0, 0)
+  GL.glVertex3f(0, 1, 0)
+	GL.glEnd()
+end
+
+local event = ffi.new("SDL_Event")
+local looping = true
+
+local mouse_position={0,0}
+local mouse_down=false
+
+while looping do
+
+  while SDL.SDL_PollEvent(event) ~= 0 do
+    if event.type == SDL.SDL_QUIT or
+    ( event.type == SDL.SDL_KEYDOWN and event.key.keysym.sym == SDL.SDLK_ESCAPE ) 
+    then
+        looping = false
+
+    elseif event.type == SDL.SDL_MOUSEBUTTONDOWN then
+      mouse_down = true
+
+    elseif event.type == SDL.SDL_MOUSEBUTTONUP then
+      mouse_down = false
+      
+    elseif event.type == SDL.SDL_MOUSEMOTION then
+      mouse_position = {event.button.x, event.button.y}
+    end
+  end
+
+  update(mouse_position,mouse_down)
+
+  draw()
+
+  SDL.SDL_GL_SwapWindow(window)
+end
+
+SDL.SDL_Quit()
